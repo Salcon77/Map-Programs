13d83c235c5dcb5599c8461a6841e62cbc37b73d
Salcon77 <Salcon77@users.noreply.github.com>
2018-11-02 18:48:46 -0500

    auto-formatting
    ctrl alt L

--- src/CheckSum.java
+++ src/CheckSum.java
@@ -2,10 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckSum {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
 
-            int Array[]= {1,2,3,4,5,6,7,8,9,10};
-            int Arrayx[]={-1,-2,-3,1,2,3};
+        int Array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int Arrayx[] = {-1, -2, -3, 1, 2, 3};
 
         CheckSum(Array, 11);
         CheckSum(Array, 100);
@@ -13,29 +13,29 @@ public class CheckSum {
 
     }
 
-    public static void CheckSum(int array[], int sum){
+    public static void CheckSum(int array[], int sum) {
 
         /*Create a map with a integer integer key value pair where the key represents the int in the array and the value
-       represents the difference needed  to equal the sum.*/
-        Map<Integer,Integer> ValueAndDifference = new HashMap<>();
+        represents the difference needed  to equal the sum.*/
+        Map<Integer, Integer> ValueAndDifference = new HashMap<>();
         boolean x = false;
 
         // This loop iterates once for each index in the given array. This loop populates the HashMap.
-        for(int i=0;i<array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             /* We find the difference needed at each array index to add up to the sum.Then we assign all indexes of
               the array as keys and their difference from the sum as values in the HashMap.*/
-            int difference = sum-array[i];
+            int difference = sum - array[i];
             ValueAndDifference.put(array[i], difference);
 
             //Now we check to see if map contains the difference as key.
-            if(ValueAndDifference.containsKey(difference)){
+            if (ValueAndDifference.containsKey(difference)) {
                 x = true;
-                System.out.println("The two values in the array that add up to "+sum+" are "+ValueAndDifference.get(array[i])+" and "+ValueAndDifference.get(difference));
+                System.out.println("The two values in the array that add up to " + sum + " are " + ValueAndDifference.get(array[i]) + " and " + ValueAndDifference.get(difference));
 
             }
         }
-        if(x==false){
-            System.out.println("No two values in the array add up to"+ sum);
+        if (x == false) {
+            System.out.println("No two values in the array add up to" + sum);
         }
 
 
--- src/FirstNonRepeating.java
+++ src/FirstNonRepeating.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class FirstNonRepeating {
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
        /* The FirstNonRepeatingChar method doesn't take blank spaces into considerations so
           you can pass  phrase like "hello hello" as the string parameter and it will print
           that all chars in it repeat instead of saying the first non repeating char is a blank space ' '
@@ -32,17 +32,15 @@ public class FirstNonRepeating {
 
             /* This if statement checks to see if the current indexed character ->.charAt(I)
                 is already in the hashMap ->.containskey() */
-            if (charMap.containsKey(FNRstring.charAt(i)) ) {
+            if (charMap.containsKey(FNRstring.charAt(i))) {
 
                 /*If the indexed char is in the hashMap already then we will reassign its keys value to be incremented by 1.
                  This is possible because hashMaps do not allow for duplicates.
                  The value here is acting as a counter which we will later be able to check to see how many times
                  each character occurs in the given string*/
-                charMap.put(FNRstring.charAt(i), charMap.get(FNRstring.charAt(i))+1);
+                charMap.put(FNRstring.charAt(i), charMap.get(FNRstring.charAt(i)) + 1);
 
-            }
-
-            else {
+            } else {
                 // If the map does not contain the indexed character then we add it to the hashMap with a value of 1
                 charMap.put(FNRstring.charAt(i), 1);
             }
@@ -52,7 +50,7 @@ public class FirstNonRepeating {
         /* At this point we have created a hashMap with all the characters in the given string and their values
            representing how many times they occur in the given string. All we need to do now is  find the first instance
            of a character having the value of 1 in the map */
-        int x = -1 ;
+        int x = -1;
         for (int j = 0; j < FNRstring.length(); j++) {
             // !=' ' makes it so we ignore blank spaces
             if (charMap.get(FNRstring.charAt(j)) == 1 && FNRstring.charAt(j) != ' ') {
@@ -63,12 +61,10 @@ public class FirstNonRepeating {
 
         }
 
-        if( x != -1){
-            System.out.println ( "the first non repeating charachter in \"" + FNRstring + "\" is " + FNRstring.charAt(x));
-        }
-
-        else{
-            System.out.println("all chars in \""+FNRstring+"\" repeat");
+        if (x != -1) {
+            System.out.println("the first non repeating charachter in \"" + FNRstring + "\" is " + FNRstring.charAt(x));
+        } else {
+            System.out.println("all chars in \"" + FNRstring + "\" repeat");
         }
     }
 }
